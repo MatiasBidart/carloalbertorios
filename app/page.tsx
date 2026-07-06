e505364574f993e097a07437bde2a909dc0cf71b
Matias Bidart <matiasezequielbidart@gmail.com>
2026-07-06 19:10:13 -0300

    FE 0.6 cambiosFavicon

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ const services = [
     id:"0",
     icon:  (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-users-icon lucide-users"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><path d="M16 3.128a4 4 0 0 1 0 7.744"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><circle cx="9" cy="7" r="4"/></svg>),
     title: "DELITOS CONTRA LAS PERSONAS",
-    description: (<div>Defensa penal ante amenazas, denuncias injustas o exageradas o <p className="text-purple-800 font-bold ">violencia de género.</p></div>)
+    description: (<div>Defensa penal ante amenazas, denuncias injustas y <p className="text-purple-800 font-bold ">violencia de género.</p></div>)
   },
   {
     id:"7",
